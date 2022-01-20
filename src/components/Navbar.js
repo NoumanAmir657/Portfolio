6b8d7444c9a2958de5ca10f680038931694212ec
Nouman Amir <noumanamir453@gmail.com>
2022-01-21 01:10:56 +0500

    Added color changing in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [color, setColor] = useState(['#FFFFFF', '#FFFFFF', '#FFFFFF'])
+
   return (
     <header className="md:sticky top-0 z-10" style={{backgroundColor: "#e52165", fontFamily:"Architects Daughter"}}>
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <div className="title-font font-medium text-white mb-4 md:mb-0 rounded-xl py-1" style={{backgroundColor: "#0d1137"}}>
+        <div className="title-font font-medium text-white mb-4 md:mb-0 rounded-xl py-1" style={{backgroundColor: "#0d1137"}} onClick={() => setColor(['#FFFFFF', '#FFFFFF', '#FFFFFF'])}>
           <Link to='/'  className="px-4 text-xl">
             Nouman Amir
           </Link>
         </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <Link to='/projects' className="mr-5 text-white text-lg font-bold" style={{color: "#0d1137"}}>
+          <Link onClick={() => setColor(['#0d1137', '#FFFFFF', '#FFFFFF'])} to='/projects' className="mr-5 text-white text-lg font-semibold" style={{color: `${color[0]}`}}>
             Projects
           </Link>
-          <Link to="/skills" className="mr-5 text-white text-lg">
+          <Link onClick={() => setColor(['#FFFFFF', '#0d1137', '#FFFFFF'])} to="/skills" className="mr-5 text-white text-lg font-semibold" style={{color: `${color[1]}`}}>
             Skills
           </Link>
-          <Link to='/education' className="mr-5 text-white text-lg">
+          <Link onClick={() => setColor(['#FFFFFF', '#FFFFFF', '#0d1137'])} to='/education' className="mr-5 text-white text-lg font-semibold" style={{color: `${color[2]}`}}>
             Education
           </Link>
         </nav>
@@ -25,6 +27,8 @@ export default function Navbar() {
     </header>
   );
 }
+
+export default Navbar
 
 //Dumps
 //1e2761
