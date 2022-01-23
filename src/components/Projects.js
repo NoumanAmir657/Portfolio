badb6980a0438b98994ee33174d1e3ba99640b09
Nouman Amir <noumanamir453@gmail.com>
2022-01-23 15:35:38 +0500

    Started project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Projects.css'
+import dec from '../assets/742762.png'
 
 const Projects = () => {
   return (
-    <div>
-        In Projects
+    <div className="h-full bg-fixed overflow-y-auto text-white" style={{backgroundImage: `linear-gradient(to right, rgba(13, 17, 55, 1), rgba(117, 19, 93, 1))`, fontFamily:"Architects Daughter"}}>
+      <div className='animationFade text-center font-semibold text-6xl pt-10' style={{textShadow: "4px 4px #0d1137"}}>
+        My Projects
+      </div>
+
+      <div className='mr-auto ml-auto w-3/4 mt-10'>
+        <div className='block justify-between lg:flex'>
+          <div>
+            <h1 className='text-5xl my-3' style={{textShadow: "4px 4px rgba(117, 19, 93, 0.73)"}}>Explore</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <img className='mx-auto mt-5 lg:mx-10 rounded-xl' src={dec} width='500px' height='500px'></img>
+        </div>
+      </div>
+
     </div>
   );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,12 +7,12 @@ const Skills = () => {
     <div className="h-full bg-fixed" style={{backgroundImage: `linear-gradient(to right, rgba(13, 17, 55, 1), rgba(117, 19, 93, 1))`, fontFamily:"Architects Daughter"}}>
       <ParticleContainer/>
       <div style={{position: "absolute", top: 0, right: 0, left: 0, bottom: 0}}>
-      <div className='animationFade text-center text-white font-semibold text-6xl pt-32' style={{textShadow: "4px 4px #0d1137"}}>
+      <div className='animationFade text-center text-white font-semibold text-6xl pt-36 lg:pt-32 ' style={{textShadow: "4px 4px #0d1137"}}>
         My Skills
       </div>
 
-      <div className='glass mt-20 w-1/2 ml-auto mr-auto'>
-      <div className="flex-center">
+      <div className='glass mt-20 w-full ml-auto mr-auto lg:w-1/2'>
+      <div className="flex-center flex-wrap">
         <i class="devicon-react-original colored icon-3d" style={{fontSize: "70px"}}></i>
         <i class="devicon-mongodb-plain-wordmark colored icon-3d" style={{fontSize: "70px"}}></i>
         <i class="devicon-nodejs-plain-wordmark colored icon-3d" style={{fontSize: "85px"}}></i>
@@ -20,7 +20,7 @@ const Skills = () => {
         <i class="devicon-mysql-plain-wordmark icon-3d" style={{fontSize: "85px"}}></i>
       </div>
 
-      <div className="flex-center mt-5">
+      <div className="flex-center mt-5 flex-wrap">
         <i className="devicon-vscode-plain colored icon-3d" style={{fontSize: "70px"}}></i>
         <i className="devicon-git-plain colored icon-3d" style={{fontSize: "70px"}}></i>
         <i className="devicon-cplusplus-plain colored icon-3d" style={{fontSize: "70px"}}></i>
